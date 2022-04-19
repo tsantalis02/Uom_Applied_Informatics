1cc10cac4b8b14d65855eb61cf871d982b6bebe5
tsantalis02 <103073986+tsantalis02@users.noreply.github.com>
2022-04-19 23:48:44 +0300

    Update a20f3.c

--- Procedural_Programming/f3/a20f3.c
+++ Procedural_Programming/f3/a20f3.c
@@ -1,16 +1,26 @@
+/*
+File:a20f3.c
+Γράψτε ένα πρόγραμμα που να διαβάζει μια σειρά από ηλικίες ανθρώπων (ακολουθία αριθμών int που εισάγεται από το χρήστη)
+και να βρίσκει την μικρότερη και τη μεγαλύτερη ηλικία. Το πρόγραμμα θα συνεχίζει να διαβάζει ηλικίες μέχρι να δοθεί ως ηλικία
+ο αριθμός -1
+*/
+
 #include <stdio.h>
 #include "genlib.h"
 #include "simpio.h"
 #include <math.h>
 
 int main(){
-int ilikia,max,min;
-printf("Dwse ilikia: ");
-ilikia=GetInteger();
+/*Δήλωση-Αρχικοποίηση μεταβλητών*/
+ int ilikia,max,min;
+/*Εισαγωγή δεδομένων*/ 
+ printf("Dwse ilikia: ");
+ ilikia=GetInteger();
  if (ilikia>=0){
-    max=ilikia;
-    min=ilikia;
+    max=ilikia; /* Θέτουμε ως μέγιστη ηλικία, τον αριθμό που εισήχθη από τον χρήστη */
+    min=ilikia; /* Θέτουμε ως ελάχιστη ηλικία, τον αριθμό που εισήχθη από τον χρήστη */
  }
+ /*Υπολογισμοί*/
  while(ilikia>=0){
     printf("Dwse ilikia: ");
     ilikia=GetInteger();
@@ -23,7 +33,8 @@ ilikia=GetInteger();
        }
      }
  }
-printf("H megalyteri ilikia einai %d\n",max);
-printf("H mikroteri ilikia einai  %d\n",min);
-return 0;
+  /*Εκτύπωση αποτελεσμάτων*/
+  printf("H megalyteri ilikia einai %d\n",max);
+  printf("H mikroteri ilikia einai  %d\n",min);
+  return 0;
 }
