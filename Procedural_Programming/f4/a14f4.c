1b166e653aed0c78b5e7f8aa5c332994e8c7aef7
tsantalis02 <103073986+tsantalis02@users.noreply.github.com>
2022-04-19 23:59:48 +0300

    Update a14f4.c

--- Procedural_Programming/f4/a14f4.c
+++ Procedural_Programming/f4/a14f4.c
@@ -1,3 +1,14 @@
+/*
+File:a14f4.c
+ Να γίνει πρόγραμμα το οποίο:
+	1) Θα διαβάζει τις τιμές τριών ακεραίων αριθμών a, b, c,
+	2) Θα υπολογίζει και θα εμφανίζει την τιμή της παράστασης (double):
+	 όπου max(a, b) είναι η συνάρτηση υπολογισμού του μεγίστου των αριθμών a και b
+	 και gr(a, b, c) η συνάρτηση υπολογισμού του μεγίστου τριών αριθμών a, b, c. Το
+	 διάβασμα των τριών αριθμών, ο υπολογισμός και εμφάνιση της τιμής της παράστασης θα
+	 γίνουν στο κυρίως πρόγραμμα, χρησιμοποιώντας τις συναρτήσεις max και gr που θα
+	 υλοποιήσετε ως συναρτήσεις στην C. Το αποτέλεσμα (y) θα εμφανίζεται με ακρίβεια 2 δεκαδικών ψηφίων.
+*/
 #include <stdio.h>
 #include "genlib.h"
 #include "simpio.h"
@@ -7,20 +18,23 @@ int max (int a,int b);
 int gr (int a,int b,int c);
 
 int main(){
-int a,b,c;
-double y;
-printf ("Dwse a :");
-a=GetInteger();
-printf ("Dwse b :");
-b=GetInteger();
-printf ("Dwse c :");
-c=GetInteger();
-y=((2*max(a,b)+3*gr(a,b,c))/4.0);
-printf ("y = % .2f \n",y);
-return 0;}
+ int a,b,c;
+ double y;
+ 
+ printf ("Dwse a :");
+ a=GetInteger();
+ printf ("Dwse b :");
+ b=GetInteger();
+ printf ("Dwse c :");
+ c=GetInteger();
+ //Υπολογισμός
+ y=((2*max(a,b)+3*gr(a,b,c))/4.0);
+ //Έξοδος
+ printf ("y = % .2f \n",y);
+ return 0;}
 
 int max(int a,int b){
-int megisto;
+ int megisto;
  if (a>b){
     megisto=a;
  }else{
@@ -30,9 +44,10 @@ return megisto;}
 
 
 int gr(int a,int b,int c){
-int maximum;
+ int maximum;
  maximum=max(a,b);
   if(c>maximum){
     maximum=c;
   }
-return maximum;}
+return maximum;
+}
