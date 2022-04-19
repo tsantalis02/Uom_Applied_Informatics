e9cc56149bdb9def1ce2e2e69d5a8f0d6287e7d4
tsantalis02 <103073986+tsantalis02@users.noreply.github.com>
2022-04-19 23:25:35 +0300

    Update a13f2.c

--- Procedural_Programming/f2/a13f2.c
+++ Procedural_Programming/f2/a13f2.c
@@ -1,12 +1,24 @@
+/*
+File:a13f2.c
+    Μία μηχανή αυτόματης πώλησης καφέ δέχεται μόνο ΕΝΑ χαρτονόμισμα των 5 € κάθε φορά και δίνει τα ρέστα σε κέρματα
+    των 10, 20 & 50 λεπτών καθώς και σε κέρματα του 1 και 2 ευρώ . Κάθε καφές κοστίζει 70 λεπτά.
+    Να γραφεί πρόγραμμα το οποίο να ζητάει τον αριθμό των καφέδων (int) και να εμφανίζει τα ρέστα (σε κέρματα) (int)
+    που πρέπει να δώσει από το χαρτονόμισμα των 5€. Στην περίπτωση που τα χρήματα δεν επαρκούν εμφανίζει το κατάλληλο μήνυμα.
+    (Τα χρήματα υπολογίζονται σε λεπτά του ευρώ).
+*/
+
 #include <stdio.h>
 #include "genlib.h"
 #include "simpio.h"
 
 int main(){
- int kafedes,kostos,x,y,z,n,m,ypoloipo;
- printf("Dwse kafedes :");
- kafedes=GetInteger();
- kostos=kafedes*70;
+ /*Δήλωση και αρχικοποίηση μεταβλητών*/
+  int kafedes,kostos,x,y,z,n,m,ypoloipo;
+ /*Εισαγωγή δεδομένων*/
+  printf("Dwse kafedes :");
+  kafedes=GetInteger();
+ /*Υπολογισμοί και εκτύπωση αποτελέσματος*/ 
+  kostos=kafedes*70;
   if (kostos>500){
     printf("Den ftanoun ta xrhmata gia tosous kafedes");
   }else{
@@ -28,5 +40,6 @@ int main(){
    printf("Kermata 20L :%d\n",n);
    printf("Kermata 10L :%d\n",m);
   }
-return 0;
+ 
+  return 0;
 }
