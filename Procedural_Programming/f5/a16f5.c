cd3ec4d8adf3e70c2a31239797e2e3f1fd4a6d1f
tsantalis02 <103073986+tsantalis02@users.noreply.github.com>
2022-04-20 00:31:13 +0300

    Update a16f5.c

--- Procedural_Programming/f5/a16f5.c
+++ Procedural_Programming/f5/a16f5.c
@@ -1,3 +1,15 @@
+/*
+File:a16f5.c
+ Να αναπτυχθεί (με τη χρήση των συναρτήσεων που περιγράφονται) πρόγραμμα στο οποίο,
+    - θα γεμίζει τον πίνακα a μεγέθους 50 με τυχαίους αριθμούς από το 0 έως το 9 χρησιμοποιώντας την
+    συνάρτηση rand() (a[i]=rand() % 10), της βιβλιοθήκης <stdlib.h> (συνάρτηση populate)
+    - θα εμφανίζει τον πίνακα a στην οθόνη (συνάρτηση printArr),
+    - θα ζητά έναν ακέραιο αριθμό Ν από το 0 έως και το 9 από τον χρήστη - δεν απαιτείται έλεγχος,
+    - θα καλεί την συνάρτηση checkTable, η οποία θα υπολογίζει τον αριθμό των εμφανίσεων του αριθμού Ν στον πίνακα a
+    και τις θέσεις του πίνακα στις οποίες εμφανίζεται ο αριθμός
+    - θα εμφανίζει τα αποτελέσματα στο χρήστη, όπως φαίνεται στο παράδειγμα εκτέλεσης. Η εμφάνιση των αποτελεσμάτων
+    θα γίνεται από τη συνάρτηση main() (κυρίως πρόγραμμα), και των θέσεων που εμφανίζεται ο αριθμός Ν καλώντας την printArr.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "genlib.h"
@@ -11,22 +23,26 @@ int checkTable(int size,int a[],int N,int T[]);
 
 int main()
 {
-int size=50,a[size],N,plithos,T[plithos];
-populate(size,a);
-printArr(size,a);
-printf("Dwse arithmo apo to 1 ews to 9: ");
-N=GetInteger();
-plithos=checkTable(size,a,N,T);
-printf("O arithmos %d emfanisthke %d fores. \n",N,plithos);
-printf("Stis Theseis: \n");
-printArr(plithos,T);
-return 0;
+ int size=50,a[size],N,plithos,T[plithos];
+ /*Δημιουργία πίνακα*/
+ populate(size,a);
+ /*Εκτύπωση αρχικού πίνακα*/ 
+ printArr(size,a);
+ /*Εισαγωγή δεδομένων*/ 
+ printf("Dwse arithmo apo to 1 ews to 9: ");
+ N=GetInteger();
+ plithos=checkTable(size,a,N,T);
+ /*Εκτύπωση αποτελεσμάτων*/ 
+ printf("O arithmos %d emfanisthke %d fores. \n",N,plithos);
+ printf("Stis Theseis: \n");
+ printArr(plithos,T);
+ return 0;
 }
 
 
 void populate(int row,int A[])
 {
-int i;
+ int i;
   for(i=0;i<row;i++)
   {
       A[i]=rand() % 10 ;
@@ -35,17 +51,17 @@ int i;
 
 void printArr(int row,int A[])
 {
-int i;
+ int i;
     for(i=0;i<row;i++)
     {
         printf("%7d",A[i]);
     }
-printf("\n");
+ printf("\n");
 }
 
 int checkTable(int row,int A[],int N,int K[])
 {
-int i,r=0;
+ int i,r=0;
     for(i=0;i<row;i++)
     {
         if(A[i]==N)
@@ -54,5 +70,5 @@ int i,r=0;
           K[r]=i;
        }
    }
-return r;
+ return r;
 }
