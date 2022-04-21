b37e9e65c491165f42e5f44ba6d52969960814e0
tsantalis02 <103073986+tsantalis02@users.noreply.github.com>
2022-04-22 01:11:48 +0300

    Update a1f4.c

--- Data_Structures/f4/a1f4.c
+++ Data_Structures/f4/a1f4.c
@@ -1,13 +1,44 @@
+/*
+File:a1f4.c
+   
+Άδεια χρήσης: GNU General Public License v3.0
+    
+    Να υλοποιηθεί η συνάρτηση Search με το παρακάτω πρωτότυπο:
+    boolean Search(ListPointer FreePtr, ListPointer List, NodeType Node[NumberOfNodes], ListElementType Item, ListPointer *PredPtr);
+    
+    η οποία θα δέχεται μια Συνδεδεμένη Λίστα (ΣΛ) υλοποιημένη με πίνακα (παράμετροι FreePtr, List, Node) και ένα στοιχείο
+    Item και θα επιστρέφει boolean τιμή η οποία δείχνει αν βρέθηκε ή όχι το αναζητούμενο στοιχείο Item στη ΣΛ και τη θέση
+    (PredPtr) του στοιχείου που είναι προηγούμενο του Item, ώστε η ΣΛ που θα προκύψει μετά την εισαγωγή ή τη διαγραφή
+    του στοιχείου Item να είναι ταξινομημένη (χρήση της Search πριν την εισαγωγή ή τη διαγραφή στοιχείου στη ΣΛ). Η
+    εισαγωγή στοιχείων στην Σ.Λ. γίνεται επαναληπτικά και ελέγχεται από το μήνυμα ‘Continue Y/N:’ (δες στιγμιότυπο
+    εκτέλεσης). Στη ΣΛ καταχωρούνται ακέραιοι. H συνάρτηση Search θα κληθεί 2 φορές μια για στοιχείο που υπάρχει στη ΣΛ
+    και μια για στοιχείο που δεν υπάρχει στη ΣΛ. Αν το στοιχείο βρεθεί θα εμφανίζει το μήνυμα 
+    «The number is in the list and its predecessor is in position» και την τιμή της PredPtr, διαφορετικά θα εμφανίζει το μήνυμα 
+    «The number is not in the list». Το πρόγραμμα θα πρέπει να εκτελεί κατά σειρά τις παρακάτω λειτουργίες και οι κλήσεις 
+    των συναρτήσεων θα γίνονται από τη main().
+    A. Αρχικοποίηση storage pool
+    B. Δημιουργία ΑΣΛ (μέγιστο μέγεθος 10)
+    C. Εμφάνιση της storage pool
+    D. Εμφάνιση των στοιχείων της ΣΛ
+    E. Εισαγωγή στοιχείων στην ΣΛ. Μετά από κάθε εισαγωγή στοιχείου στη ΣΛ αυτή θα πρέπει να παραμένει 
+    ταξινομημένη σε αύξουσα διάταξη (χρήση της Search πριν την εισαγωγή στοιχείου στη ΣΛ)
+    F. Εμφάνιση της storage pool
+    G. Εμφάνιση των στοιχείων της ΣΛ
+    H. Έλεγχος εάν η ΣΛ είναι άδεια. Αν η ΣΛ είναι άδεια εμφανίζει μήνυμα «Empty List» διαφορετικά «Not an Empty List»
+    I. Έλεγχος εάν η ΣΛ είναι γεμάτη. Αν η ΣΛ είναι γεμάτη εμφανίζει μήνυμα «Full List» διαφορετικά «Not a Full List»
+    J. Αναζήτηση ενός στοιχείου στη ΣΛ (για στοιχείο που υπάρχει στη ΣΛ και για στοιχείο που δεν υπάρχει).
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-#define NumberOfNodes 10
+#define NumberOfNodes 10   /*όριο μεγέθους της συνδεδεμένης λίστας */
 
-#define NilValue -1
+#define NilValue -1         // ειδική μεδενικη τιμη δείχνει το τέλος της Συνδ.λίστας
 
-typedef int ListElementType;
-
+typedef int ListElementType; /*τύπος δεδομένων για τα στοιχεία της συνδεδεμένης λίστας, 
+                                ενδεικτικά επιλέχθηκε ο τύπος int */
 typedef int ListPointer;
 
 typedef struct {
@@ -36,11 +67,15 @@ int main(){
   ListElementType Item;
   boolean found;
   char ch;
+   // Ερώτημα A
    InitializeStoragePool(N,&ptr);
+   // Ερώτημα B
    CreateList(&L);
+   // Ερώτημα C 
    printf("-----------Question C----------\n");
    printf("-----------Storage pool--------\n");
    printAll(L,ptr,N);
+   // Ερώτημα D 
    printf("-----------Question D-----------\n");
    printf("-----------Linked list----------\n");
    TraverseLinked(L,N);
@@ -56,24 +91,29 @@ int main(){
         scanf("%c",&ch);
      }while( toupper(ch)!='Y' && toupper(ch)!='N' );
    }while(toupper(ch)!='N');
+   // Ερώτημα F 
    printf("-----------Question F------------\n");
    printf("-----------Storage pool----------\n");
    printAll(L,ptr,N);
+   // Ερώτημα G 
    printf("-----------Question G------------\n");
    printf("-----------Linked list-----------\n");
    TraverseLinked(L,N);
+    // Ερώτημα H
    printf("----------Question H------------\n");
      if(EmptyList(L)){
         printf("Empty List\n");
      }else{
         printf("Not an Empty List\n");
     }
+    // Ερώτημα I
    printf("-----------Question I------------\n");
     if(FullList(ptr)){
         printf("Full List\n");
     }else{
         printf("Not a Full List\n");
     }
+    // Ερώτημα J
    printf("-----------Question J------------\n");
     for(i=0;i<=1;i++){
         printf("Give a number ");
@@ -89,6 +129,14 @@ int main(){
 }
 
 void InitializeStoragePool(NodeType Node[], ListPointer *FreePtr)
+/* Δέχεται:   Τον πίνακα Node και τον δείκτη FreePtr που δείχνει στον
+                πρώτο διαθέσιμο κόμβο.
+  Λειτουργία: Αρχικοποιεί τον πίνακα Node ως συνδεδεμένη λίστα συνδέοντας μεταξύ
+                τους διαδοχικές εγγραφές του πίνακα,
+                και αρχικοποιεί τον δείκτη FreePtr .
+  Επιστρέφει: Τον τροποποιημένο πίνακα Node και τον
+                δείκτη FreePtr του πρώτου διαθέσιμου κόμβου
+*/
 {
    int i;
 
@@ -103,22 +151,37 @@ void InitializeStoragePool(NodeType Node[], ListPointer *FreePtr)
 }
 
 void CreateList(ListPointer *List)
-
+/* Λειτουργία: Δημιουργεί μια κενή συνδεδεμένη λίστα.
+   Επιστρέφει: Έναν (μηδενικό) δείκτη που δείχνει σε κενή λίστα
+*/
 {
   *List=NilValue;
 }
 
 boolean EmptyList(ListPointer List)
+/* Δέχεται:    Έναν δείκτη List που δείχνει σε μια συνδεδεμένη λίστα.
+   Λειτουργία: Ελέγχει αν η συνδεδεμένη λίστα είναι κενή.
+   Επιστρέφει: True αν η συνδεδεμένη λίστα είναι κενή και false διαφορετικά
+*/
 {
   return (List==NilValue);
 }
 
 boolean FullList(ListPointer FreePtr)
+/* Δέχεται:    Μια συνδεδεμένη λίστα.
+   Λειτουργία: Ελέγχει αν η συνδεδεμένη λίστα είναι γεμάτη.
+   Επιστρέφει: True αν η συνδεδεμένηλίστα είναι γεμάτη, false διαφορετικά
+*/
 {
   return (FreePtr == NilValue);
 }
 
 void GetNode(ListPointer *P, ListPointer *FreePtr, NodeType Node[])
+/* Δέχεται:   Τον πίνακα Node και τον δείκτη FreePtr.
+  Λειτουργία: Αποκτά έναν "ελεύθερο" κόμβο που τον δείχνει ο δείκτης P.
+  Επιστρέφει: Τον δείκτη P και τον τροποποιημένο  δείκτη FreePtr
+                που δεικτοδοτεί στο πρώτο διαθέσιμο κόμβο
+*/
 {
   *P = *FreePtr;
   if (!FullList(*FreePtr))
@@ -127,6 +190,14 @@ void GetNode(ListPointer *P, ListPointer *FreePtr, NodeType Node[])
 
 
 void Insert(ListPointer *List, NodeType Node[],ListPointer *FreePtr, ListPointer PredPtr, ListElementType Item)
+/* Δέχεται:    Μια συνδεδεμένη λίστα, τον πίνακα Node, τον δείκτη PredPtr και
+                ένα στοιχείο Item.
+  Λειτουργία: Εισάγει στη συνδεδεμένη λίστα, αν δεν είναι γεμάτη, το στοιχείο
+                Item μετά από τον κόμβο στον οποίο δείχνει ο δείκτης PredPtr.
+  Επιστρέφει: Την τροποποιημένη συνδεδεμένη λίστα, τον τροποποιημένο πίνακα Node
+                και τον δείκτη FreePtr.
+  Εξοδος:     Μήνυμα γεμάτης λίστας, αν η συνδεδεμένη λίστα είναι γεμάτη
+*/
 {
   ListPointer TempPtr;
   GetNode(&TempPtr,FreePtr,Node);
@@ -150,9 +221,11 @@ void Insert(ListPointer *List, NodeType Node[],ListPointer *FreePtr, ListPointer
 
 
 void TraverseLinked(ListPointer List, NodeType Node[])
+/* Δέχεται:    Μια συνδεδεμένη λίστα.
+   Λειτουργία: Κάνει διάσχιση της συνδεδεμένης λίστας, αν δεν είναι κενή.
+   Έξοδος:     Εξαρτάται από την επεξεργασία
+*/
 {
-
-
   ListPointer CurrPtr;
 
   if (!EmptyList(List))
