41789d750da0ee1e5ea2f5b25770131b08ac3a20
tsantalis02 <103073986+tsantalis02@users.noreply.github.com>
2022-04-22 02:39:36 +0300

    Update a30f5.c

--- Data_Structures/f5/a30f5.c
+++ Data_Structures/f5/a30f5.c
@@ -1,14 +1,29 @@
+/*
+File:a30f5.c
+   
+Άδεια χρήσης: GNU General Public License v3.0
+    
+    Το αρχείο transactions.txt περιλαμβάνει ύποπτες συναλλαγές και θέλουμε να εντοπίσουμε τις m μεγαλύτερες συναλλαγές.
+    Η διαθέσιμη μνήμη δεν επαρκεί για να διαβάσουμε όλες τις συναλλαγές από το αρχείο και να τις αποθηκεύσουμε σε μια
+    δομή δεδομένων στη μνήμη. Επιλέξτε την κατάλληλη δομή δεδομένων ώστε να βρίσκει τις m μεγαλύτερες συναλλαγές.
+    Μετά την εύρεση των m μεγαλύτερων συναλλαγών θα εμφανίζει το μέγεθος και τα στοιχεία της δομής δεδομένων
+    (προσαρμόστε κατάλληλα την PrintHeap) και στη συνέχεια θα εμφανίσει σε αύξουσα διάταξη τις m μεγαλύτερες
+    συναλλαγές. Την τιμή της m θα τη δίνει ο χρήστης, και θεωρήστε ότι δίνεται τιμή πολύ μικρότερη του μεγέθους του
+    αρχείου χωρίς να γίνεται σχετικός έλεγχος. Ως δομή δεδομένων θα πρέπει να χρησιμοποιηθεί σωρός (μέγιστος ή ελάχιστος
+    σωρός;).
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-#define MaxElements 16
+#define MaxElements 16         //το μέγιστο πλήθος των στοιχείων του σωρού
 
-typedef double  HeapElementType;
+typedef double  HeapElementType;    //ο τύπος δεδομένων των στοιχείων του σωρού
 typedef  struct {
      HeapElementType key;
-    // int Data;
+    // int Data;                // οποιοσδήποτε τύπος για τα παρελκόμενα δεδομένα κάθε κόμβου
 } HeapNode;
 
 
@@ -34,43 +49,59 @@ HeapNode AnItem;
 int  m;
 FILE* fp;
  printf("Give m:");
- scanf("%d",&m);
- CreateMinHeap(&AHeap);
- fp=fopen("transactions.txt","r");
-  while(!feof(fp)){
-    fscanf(fp,"%lf",&AnItem.key);
+ scanf("%d",&m); // Διάβασε το m
+ CreateMinHeap(&AHeap); // Δημιούργησε τον σωρό ελαχίστων
+ fp=fopen("transactions.txt","r"); // Άνοιξε το αρχείο για ανάγνωση
+  while(!feof(fp)){ // Όσο δεν έφτασες στο τέλος του αρχείου
+    fscanf(fp,"%lf",&AnItem.key); // Διάβασε την συναλλαγή
 
-    InsertMinHeap(&AHeap,AnItem);
-    if(AHeap.Size>m){
-        DeleteMinHeap(&AHeap,&AnItem);
+    InsertMinHeap(&AHeap,AnItem); // Εισαγωγή της συναλλαγής στον σωρό ελαχίστων
+    if(AHeap.Size>m){ // Αν το μέγεθος του σωρού ξεπέρασε το m
+        DeleteMinHeap(&AHeap,&AnItem); // Διαγραφή του ελάχιστου στοιχείου από τον σωρό
     }
   }
- PrintHeap(AHeap);
+ PrintHeap(AHeap); // Εμφάνισε το μέγεθος και το περιεχόμενο του σωρού
  printf("Transactions \n");
- while(!EmptyHeap(AHeap)){
-    DeleteMinHeap(&AHeap,&AnItem);
-    printf("%.2lf ",AnItem.key);
+ while(!EmptyHeap(AHeap)){ // Όσο ο σωρός δεν είναι κενός
+    DeleteMinHeap(&AHeap,&AnItem); // Διαγραφή του ελάχιστου στοιχείου από τον σωρό
+    printf("%.2lf ",AnItem.key); // Εμφάνιση του στοιχείου
  }
- fclose(fp);
+ fclose(fp); //Κλείσιμο αρχείου
  return 0;
 }
 
 void CreateMinHeap(HeapType *Heap)
+/* Λειτουργία: Δημιουργεί ένα κενό σωρό.
+   Επιστρέφει: Ένα κενό σωρό
+*/
 {
   (*Heap).Size=0;
 }
 
 boolean EmptyHeap(HeapType Heap)
+/* Δέχεται:    Ένα σωρό Heap.
+   Λειτουργία: Ελέγχει αν ο σωρός είναι κενός.
+   Επιστρέφει: TRUE αν ο σωρός είναι κενός, FALSE διαφορετικά
+*/
 {
   return (Heap.Size==0);
 }
 
 boolean FullHeap(HeapType Heap)
+/* Δέχεται:    Ένα σωρό.
+   Λειτουργία: Ελέγχει αν ο σωρός είναι γεμάτος.
+   Επιστρέφει: TRUE αν ο σωρός είναι γεμάτος, FALSE διαφορετικά
+*/
 {
   return (Heap.Size==MaxElements);
 }
 
 void InsertMinHeap(HeapType *Heap, HeapNode Item)
+/*  Δέχεται:    Ένα σωρό Heap και ένα στοιχείο δεδομένου Item .
+    Λειτουργία: Εισάγει το στοιχείο Item στο σωρό, αν ο σωρός δεν είναι γεμάτος.
+    Επιστρέφει: Τον τροποποιημένο σωρό.
+    Έξοδος:     Μήνυμα γεμάτου σωρού αν ο σωρός είναι γεμάτος
+*/
 {
    int hole;
 
@@ -91,6 +122,10 @@ void InsertMinHeap(HeapType *Heap, HeapNode Item)
 }
 
 void DeleteMinHeap(HeapType *Heap, HeapNode *Item)
+/* Δέχεται:    Ένα σωρό Heap.
+   Λειτουργία: Ανακτά και διαγράφει το μεγαλύτερο στοιχείο του σωρού.
+   Επιστρέφει: Το μεγαλύτερο στοιχείο Item του σωρού και τον τροποποιημένο σωρό
+*/
 {
    int parent, child;
    HeapNode last;
@@ -125,11 +160,13 @@ void DeleteMinHeap(HeapType *Heap, HeapNode *Item)
        printf("Empty heap...\n");
 }
 
+
 void PrintHeap(HeapType Heap)
 {
     int i;
     printf("Data structure size=%d \n",Heap.Size);
-    /* printf("N   L   R\n");
+    /*ΜΕ NLR 
+    printf("N   L   R\n");
      n=Heap.Size/2;
      for (i=1; i<=n;i++)
      {
@@ -144,6 +181,7 @@ void PrintHeap(HeapType Heap)
          else printf("\n");
      }*/
      //printf("Heap array\n");
+     //ΧΩΡΙΣ NLR
      for (i=1; i<=Heap.Size;i++)
         printf("%.2lf ",Heap.Element[i].key);
     printf(" \n");
