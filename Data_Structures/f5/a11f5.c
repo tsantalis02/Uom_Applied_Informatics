5ced94ebd10d109a6e63eba7054da507f32c1ff0
tsantalis02 <103073986+tsantalis02@users.noreply.github.com>
2022-04-22 01:56:19 +0300

    Update a11f5.c

--- Data_Structures/f5/a11f5.c
+++ Data_Structures/f5/a11f5.c
@@ -1,3 +1,25 @@
+/*
+File:a11f5.c
+    
+Άδεια χρήσης: GNU General Public License v3.0
+    
+    Γράψτε ένα πρόγραμμα που θα πραγματοποιεί ορθογραφικό έλεγχο ενός κειμένου. Οι λέξεις που απαρτίζουν το λεξικό
+    είναι αποθηκευμένες στο αρχείο κειμένου ‘I112f5.TXT’ (σε κάθε γραμμή του υπάρχει μία λέξη). Διαβάζονται και
+    αποθηκεύονται μία-μία σε ένα ΔΔΑ με τη συνάρτηση CreateDictionary και έτσι δημιουργείται το λεξικό-ΔΔΑ. Μετά τη
+    δημιουργία του λεξικού-ΔΔΑ θα εμφανίζετε τις λέξεις του λεξικού. Στη συνέχεια το πρόγραμμα μέσω της συνάρτησης
+    SpellingCheck θα διαβάζει ένα κείμενο που είναι αποθηκευμένο στο αρχείο κειμένου ‘I111F5.TXT’, σε κάθε γραμμή του
+    οποίου υπάρχει μία λέξη και θα διενεργεί ορθογραφικό έλεγχο, δηλαδή θα αναζητά τη λέξη του αρχείου ‘I111F5.TXT’ στο
+    λεξικό-ΔΔΑ. Κατά τον ορθογραφικό έλεγχο θα πρέπει να εκτυπώνετε τις λέξεις που δεν βρέθηκαν στο λεξικό και να
+    υπολογίζετε το πλήθος τους. Το πλήθος των λέξεων που δεν βρέθηκαν στο λεξικό θα εμφανίζεται στη main.
+    Δίνονται τα πρωτότυπα των συναρτήσεων
+    void CreateDictionary(BinTreePointer *Root, FILE *fp);
+    int SpellingCheck(BinTreePointer Root, FILE *fp);
+    
+    Η 1 παράμετρος της fopen θα πρέπει να περιλαμβάνει το όνομα του αρχείου χωρίς διαδρομή δηλαδή:
+    fp1 = fopen("i112f5.txt", "r");
+    fp2 = fopen("i111f5.txt", "r");
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -29,30 +51,46 @@ int main(){
   BinTreePointer ARoot;
   FILE* fp1;
   FILE* fp2;
+   // Άνοιγμα αρχείων
    fp1=fopen("i112f5.txt","r");
    fp2=fopen("i111f5.txt","r");
-   CreateBST(&ARoot);
-   CreateDictionary(&ARoot,fp1);
+   CreateBST(&ARoot); // Δημιουργία κενού ΔΔΑ
+   CreateDictionary(&ARoot,fp1); // Κατασκευή λεξικού
+   // Εμφάνιση του λεξικού
    printf("**********Dictionary**********\n");
    InorderTraversal(ARoot);
+   // Εμφάνιση των λέξεων που δεν είναι στο λεξικό (δηλαδή στο ΔΔΑ)
    printf("**********Not in Dictionary**********\n");
    k=SpellingCheck(ARoot,fp2);
    printf("Number of words not in Dictionary: %d\n",k);
+   // Κλείσιμο αρχείων
    fclose(fp1);
    fclose(fp2);
   return 0;
 }
 
 void CreateBST(BinTreePointer *Root)
+/* Λειτουργία: Δημιουργεί ένα κενό ΔΔΑ.
+   Επιστρέφει:  Τον μηδενικό δείκτη(NULL) Root
+*/
 {
     *Root = NULL;
 }
 
 boolean EmptyBST(BinTreePointer Root)
-{   return (Root==NULL);
+/* Δέχεται:   Ενα ΔΔα με το Root να δείχνει στη ρίζα του.
+  Λειτουργία: Ελέγχει αν το ΔΔΑ είναι κενό.
+  Επιστρέφει: TRUE αν το ΔΔΑ είναι κενό και FALSE διαφορετικά
+*/
+{   
+ return (Root==NULL);
 }
 
 void BSTInsert(BinTreePointer *Root, BinTreeElementType Item)
+/* Δέχεται:     Ένα ΔΔΑ με το δείκτη Root να δείχνει στη ρίζα του και ένα στοιχείο Item.
+   Λειτουργία:  Εισάγει το στοιχείο Item στο ΔΔΑ.
+   Επιστρέφει: Το τροποποιημένο ΔΔΑ με τον δείκτη Root να δείχνει στη ρίζα του
+*/
 {
     BinTreePointer LocPtr, Parent;
     boolean Found;
@@ -87,6 +125,12 @@ void BSTInsert(BinTreePointer *Root, BinTreeElementType Item)
 
 void BSTSearch(BinTreePointer Root, BinTreeElementType KeyValue, boolean *Found,
                 BinTreePointer *LocPtr)
+/* Δέχεται:    Ένα ΔΔΑ με το δείκτη Root να δείχνει στη ρίζα του και μια τιμή KeyValue.
+   Λειτουργία: Αναζητά στο ΔΔΑ έναν κόμβο με τιμή KeyValue στο πεδίο κλειδί του.
+   Επιστρέφει: Η Found έχει τιμή TRUE και ο δείκτης LocPtr δείχνει στον κόμβο που
+                περιέχει την τιμή KeyValue, αν η αναζήτηση είναι επιτυχής.
+                Διαφορετικά η Found έχει τιμή FALSE
+*/
 {
     (*LocPtr) = Root;
     (*Found) = FALSE;
@@ -102,6 +146,11 @@ void BSTSearch(BinTreePointer Root, BinTreeElementType KeyValue, boolean *Found,
 }
 
 void InorderTraversal(BinTreePointer Root)
+/* Δέχεται:    Ένα δυαδικό δέντρο με το δείκτη Root να δείχνει στην ρίζα του.
+   Λειτουργία: Εκτελεί ενδοδιατεταγμένη διάσχιση του δυαδικού δέντρου και
+                επεξεργάζεται κάθε κόμβο ακριβώς μια φορά.
+   Εμφανίζει: Το περιεχόμενο του κόμβου, και εξαρτάται από το είδος της επεξεργασίας
+*/
 {
     if (Root!=NULL) {
         InorderTraversal(Root->LChild);
@@ -113,9 +162,9 @@ void InorderTraversal(BinTreePointer Root)
 
 void CreateDictionary(BinTreePointer *Root,FILE* fp){
   BinTreeElementType Item;
-  while(!feof(fp)){
-    fscanf(fp,"%s",Item);
-    BSTInsert(&(*Root),Item);
+  while(!feof(fp)){ // Όσο το αρχείο δεν τελείωσε
+    fscanf(fp,"%s",Item); // Διάβασε την λέξη από το αρχείο
+    BSTInsert(&(*Root),Item); // Εισήγαγε την λέξη στο ΔΔΑ-λεξικό
   }
 }
 
@@ -124,14 +173,18 @@ int SpellingCheck(BinTreePointer Root,FILE* fp){
  BinTreePointer LocPtr;
  BinTreeElementType Item;
  int count;
-  count=0;
-  while(!feof(fp)){
-    fscanf(fp,"%s",Item);
-    BSTSearch(Root,Item,&Found,&LocPtr);
-    if(Found==FALSE){
-        printf("%s\n",Item);
-        count++;
+  count=0; // Αρχικοποίηση μετρητή λέξεων
+ 
+    // διαβάζω 1-1 λέξη του αρχείου και αναζητώ στο ΔΔΑ-λεξικό
+    // αν δεν υπάρχει στο ΔΔΑ-λεξικό τότε την εμφανίζω
+ 
+  while(!feof(fp)){ // Όσο το αρχείο δεν τελείωσε
+    fscanf(fp,"%s",Item); // Διάβασε την λέξη από το αρχείο
+    BSTSearch(Root,Item,&Found,&LocPtr); // Αναζήτησε τη λέξη στο ΔΔΑ-λεξικό
+    if(Found==FALSE){ // Αν η λέξη δεν βρέθηκε στο ΔΔΑ-λεξικό
+        printf("%s\n",Item); // Εμφάνισε την λέξη
+        count++; // Αύξησε τον μετρητή
     }
   }
-  return count;
+  return count; // Επέστρεψε τον μετρητή
 }
